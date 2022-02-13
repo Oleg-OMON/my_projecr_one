5e9a534d6cf621eb6d62ceaa1e87c8da4e98f51f
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-02-13 19:01:21 +0300

    file added

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, profile
+from .views import index, other_page, BBLoginView, profile, BBLogoutView
 
 app_name = 'main'
 urlpatterns = [
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
-    path('accounts/login', BBLoginView.as_view(), name='login'),
-    path('accounts/login', profile, name='profile'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
 ]
